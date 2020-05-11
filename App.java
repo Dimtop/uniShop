443310c7f7a7b7124cb6a779b7a19b0543085fbe
Dimtop <topalidisdofcl@gmail.com>
2020-05-11 20:07:34 +0300

    Update

--- App.java
+++ App.java
@@ -1,12 +1,16 @@
 package dbconnector;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
-public class App 
-{
-    public static void main(String args[]){
+public class App {
+    public static void main(String args[]) {
 
         UserController uc = new UserController();
+
+
+        String registerResult = uc.registerUser("tsirpanis", "123456", "tsirp@gmail.com", 0, "''", 2);
+        System.out.println(registerResult);
 
         Scanner usernameInput = new Scanner(System.in);
         System.out.println("Username: ");
@@ -18,22 +22,26 @@ public class App
 
         int loginResult = uc.authenticateUser(username, password);
 
-
-        if(loginResult == 0){
+        if (loginResult == 0) {
             System.out.println("Login failed.");
-        }
-        else{
+        } else {
             System.out.println("Loged in.");
         }
 
-
-        int usernameUpdateResult =  uc.updateUsername(1, "stampoulidisGeorgios");
+        try {
+            uc.getDBConnection().close();
+        } catch (SQLException e) {
+           
+            e.printStackTrace();
+        }
+        
+        int usernameUpdateResult =  uc.updateUsername(2, "stampoulidisGeorgios");
         
         if(usernameUpdateResult == 0){
             System.out.println("Update failed.");
         }
         else{
-            System.out.println("Uopdated.");
+            System.out.println("Updated.");
         }
     }
 }
--- UserController.java
+++ UserController.java
@@ -20,36 +20,55 @@ public class UserController {
     public String registerUser(String username, String password, String email, int premium, String preferences,
             int tier) {
 
-        if (tier < 0 || tier > 2) {
-            return "Tier should be between 1-3";
-        }
-        try {
-            this.dbStatement.executeUpdate("INSERT INTO Users VALUES (NULL,'" + username + "','" + password + "','"
-                    + email + "'," + premium + "," + "''," + preferences + "," + tier + ",''," + "''" + ");"
+            boolean userExists = false;
+            try {
+                ResultSet usersCollection = this.dbStatement.executeQuery("SELECT * FROM Users");
+    
+                while (usersCollection.next()) {
+                    if(usersCollection.getString("username").equals(username) || usersCollection.getString("email").equals(email)){
+                        userExists = true;
+                    }
+                }   
 
-            );
-            return "success";
-        } catch (Exception e) {
-            System.out.println(e);
+                if(userExists){
+                    return "The username or the email has alredy been used";
+                }
+                else{
+                    if (tier < 0 || tier > 2) {
+                        return "Tier should be between 1-3";
+                    }
+                    else{
+                        this.dbStatement.executeUpdate("INSERT INTO Users VALUES (NULL,'" + username + "','" + password + "','"
+                        + email + "'," + premium + "," + "''," + preferences + "," + tier + ",''," + "''" + ");"
+    
+                        );
+                        return "success";
+                    }
+                }
+               
+            } catch (SQLException e) {
+            
+                System.out.println(e);
 
-            return e.toString();
-        }
+                return e.toString();
+            }
     }
 
     public int authenticateUser(String username, String password) {
+
+        int userFound = 0;
 
         try {
             ResultSet usersCollection = this.dbStatement.executeQuery("SELECT * FROM Users");
 
             while (usersCollection.next()) {
+                System.out.println(usersCollection.getString("username") + usersCollection.getString("passwrd"));
                 if (usersCollection.getString("username").equals(username)
                         && usersCollection.getString("passwrd").equals(password)) {
-                    return 1;
-                } else {
-                    return 0;
+                        userFound = 1;
                 }
-            }
-            return 0;
+            }   
+            return userFound;
         } catch (SQLException e) {
         
             e.printStackTrace();
@@ -62,7 +81,7 @@ public class UserController {
 
         try {
             this.dbStatement
-                    .executeUpdate("UPDATE User SET username='" + usernameToSet + "' WHERE userID=" + userID + ";");
+                    .executeUpdate("UPDATE Users SET username='" + usernameToSet + "' WHERE userID=" + userID + ";");
             return 1;
         } catch (SQLException e) {
             
@@ -70,6 +89,10 @@ public class UserController {
             return 0;
         }
 
+    }
+
+    public Connection getDBConnection(){
+        return this.dbConnection;
     }
 
 }
