311231e9c4fbbd4e1d2ccefadbb39f55fa3391cf
Dimtop <topalidisdofcl@gmail.com>
2020-05-11 21:39:26 +0300

    Added UserModelFieldsEnum

---
+++ UserModelFieldsEnum.java
@@ -0,0 +1,13 @@
+package dbconnector;
+
+public enum UserModelFieldsEnum {
+    USERNAME,
+    PASSWORD,
+    EMAIL,
+    PREMIUM,
+    WISHLIST,
+    PREFERENCES,
+    TIER,
+    MESSAGES,
+    ADS
+}
