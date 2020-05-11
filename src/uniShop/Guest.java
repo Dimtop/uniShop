5466d0a6c07dcfda9dd25a2187d054a20526bbd2
g_st <georgios.stampoulidis@gmail.com>
2020-05-11 16:58:13 +0300

    constructors user/guest

---
+++ src/uniShop/Guest.java
@@ -0,0 +1,12 @@
+package uniShop;
+
+public class Guest extends User{
+
+	public Guest(int userId, String username, String email) {
+		super(9999, "Guest", null);
+		
+	}
+	
+	
+
+}
---
+++ src/uniShop/User.java
@@ -0,0 +1,24 @@
+package uniShop;
+
+import java.util.ArrayList;
+
+public class User {
+	
+	protected int userId;
+	protected String username;
+	protected String email;
+	protected int premium;
+	protected ArrayList<String> preferences;
+	
+	
+	public User(int userId, String username, String email) {
+		this.userId = userId;
+		this.username = username;
+		this.email = email;
+	}
+	
+	
+	
+	
+
+}
