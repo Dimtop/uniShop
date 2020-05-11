3043897bbc4844310c2f1d816c0702eadb3623c3
Dimtop <topalidisdofcl@gmail.com>
2020-05-11 21:02:27 +0300

    Update

--- App.java
+++ App.java
@@ -1,6 +1,7 @@
 package dbconnector;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
@@ -18,10 +19,12 @@ public class App {
         Scanner usernameInput = new Scanner(System.in);
         System.out.println("Username: ");
         String username = usernameInput.nextLine();
+        usernameInput.close();
 
         Scanner passwordInput = new Scanner(System.in);
         System.out.println("Password: ");
         String password = passwordInput.nextLine();
+        passwordInput.close();
 
         int loginResult = uc.authenticateUser(username, password);
 
@@ -51,6 +54,21 @@ public class App {
             System.out.println("Updated.");
         }
 
+
+        //EMAIL UPDATE
+        int emailUpdateResult = uc.updateEmail(3, "fotiosperkasKKE@gmail.com");
+        System.out.println(emailUpdateResult);
+
+        //MESSAGES UPDATE
+        ArrayList<String> dumbMessages = new ArrayList<String>();
+        dumbMessages.add("122");
+        dumbMessages.add("123");
+
+        int messagesUpdateResult = uc.updateMessages(3, dumbMessages);
+        System.out.println(messagesUpdateResult);
+
+        int deleteResult =  uc.deleteUser(5);
+        System.out.println(deleteResult);
 
         //CLOSING CONNECTION
         try {
--- UserController.java
+++ UserController.java
@@ -1,17 +1,20 @@
 package dbconnector;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class UserController {
 
     private Connection dbConnection;
     private Statement dbStatement;
+    ResultSet usersCollection;
 
     public UserController() {
         DatabaseConnector dbConnector = new DatabaseConnector();
         this.dbConnection = dbConnector.connect();
         try {
             this.dbStatement = this.dbConnection.createStatement();
+            this.usersCollection = this.dbStatement.executeQuery("SELECT * FROM Users");
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -20,38 +23,35 @@ public class UserController {
     public String registerUser(String username, String password, String email, int premium, String preferences,
             int tier) {
 
-            boolean userExists = false;
-            try {
-                ResultSet usersCollection = this.dbStatement.executeQuery("SELECT * FROM Users");
-    
-                while (usersCollection.next()) {
-                    if(usersCollection.getString("username").equals(username) || usersCollection.getString("email").equals(email)){
-                        userExists = true;
-                    }
-                }   
-
-                if(userExists){
-                    return "The username or the email has alredy been used";
+        boolean userExists = false;
+        try {
+            while (this.usersCollection.next()) {
+                if (this.searchUserByUsername(username) || this.searchUserByEmail(email)) {
+                    userExists = true;
                 }
-                else{
-                    if (tier < 0 || tier > 2) {
-                        return "Tier should be between 1-3";
-                    }
-                    else{
-                        this.dbStatement.executeUpdate("INSERT INTO Users VALUES (NULL,'" + username + "','" + password + "','"
-                        + email + "'," + premium + "," + "''," + preferences + "," + tier + ",''," + "''" + ");"
-    
-                        );
-                        return "success";
-                    }
-                }
-               
-            } catch (SQLException e) {
-            
-                System.out.println(e);
-
-                return e.toString();
             }
+
+            if (userExists) {
+                return "The username or the email has alredy been used";
+            } else {
+                if (tier < 0 || tier > 2) {
+                    return "Tier should be between 1-3";
+                } else {
+                    this.dbStatement.executeUpdate(
+                            "INSERT INTO Users VALUES (NULL,'" + username + "','" + password + "','" + email + "',"
+                                    + premium + "," + "''," + preferences + "," + tier + ",''," + "''" + ");"
+
+                    );
+                    return "success";
+                }
+            }
+
+        } catch (SQLException e) {
+
+            System.out.println(e);
+
+            return e.toString();
+        }
     }
 
     public int authenticateUser(String username, String password) {
@@ -59,17 +59,16 @@ public class UserController {
         int userFound = 0;
 
         try {
-            ResultSet usersCollection = this.dbStatement.executeQuery("SELECT * FROM Users");
 
-            while (usersCollection.next()) {
-                if (usersCollection.getString("username").equals(username)
-                        && usersCollection.getString("passwrd").equals(password)) {
-                        userFound = 1;
+            while (this.usersCollection.next()) {
+                if (this.usersCollection.getString("username").equals(username)
+                        && this.usersCollection.getString("passwrd").equals(password)) {
+                    userFound = 1;
                 }
-            }   
+            }
             return userFound;
         } catch (SQLException e) {
-        
+
             e.printStackTrace();
             return 0;
         }
@@ -79,11 +78,13 @@ public class UserController {
     public int updateUsername(int userID, String usernameToSet) {
 
         try {
+
             this.dbStatement
                     .executeUpdate("UPDATE Users SET username='" + usernameToSet + "' WHERE userID=" + userID + ";");
             return 1;
+
         } catch (SQLException e) {
-            
+
             e.printStackTrace();
             return 0;
         }
@@ -97,12 +98,203 @@ public class UserController {
                     .executeUpdate("UPDATE Users SET passwrd='" + passwordToSet + "' WHERE userID=" + userID + ";");
             return 1;
         } catch (SQLException e) {
-            
+
             e.printStackTrace();
             return 0;
         }
 
     }
+
+    public int updateEmail(int userID, String emailToSet) {
+
+        try {
+
+            this.dbStatement.executeUpdate("UPDATE Users SET email='" + emailToSet + "' WHERE userID=" + userID + ";");
+            return 1;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public int updatePremiumPlan(int userID, int premiumPlanToSet) {
+
+        if (premiumPlanToSet < 0) {
+            premiumPlanToSet = 0;
+        } else if (premiumPlanToSet > 1) {
+            premiumPlanToSet = 1;
+        }
+
+        try {
+            this.dbStatement
+                    .executeUpdate("UPDATE Users SET premium=" + premiumPlanToSet + " WHERE userID=" + userID + ";");
+            return 1;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public int updateWishlist(int userID, ArrayList<String> wishlistToSet) {
+        try {
+            this.dbStatement.executeUpdate("UPDATE Users SET wishlist='" + this.normalizeStringArrays(wishlistToSet)
+                    + "' WHERE userID=" + userID + ";");
+            return 1;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public int updatePreferences(int userID, ArrayList<String> preferencesToSet) {
+        try {
+            this.dbStatement.executeUpdate("UPDATE Users SET preferences='"
+                    + this.normalizeStringArrays(preferencesToSet) + "' WHERE userID=" + userID + ";");
+            return 1;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public int updateTier(int userID, int tierToSet) {
+
+        if (tierToSet < 0 || tierToSet > 2) {
+            return 0;
+        }
+
+        try {
+            this.dbStatement.executeUpdate("UPDATE Users SET tier=" + tierToSet + " WHERE userID=" + userID + ";");
+            return 1;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public int updateMessages(int userID, ArrayList<String> messagesToSet) {
+        try {
+            this.dbStatement.executeUpdate("UPDATE Users SET messages='" + this.normalizeStringArrays(messagesToSet)
+                    + "' WHERE userID=" + userID + ";");
+            return 1;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public int updateAds(int userID, ArrayList<String> adsToSet) {
+        try {
+            this.dbStatement.executeUpdate(
+                    "UPDATE Users SET ads='" + this.normalizeStringArrays(adsToSet) + "' WHERE userID=" + userID + ";");
+            return 1;
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public int deleteUser(int userID) {
+        try {
+            this.dbStatement.executeUpdate("DELETE FROM Users WHERE userID=" + userID + ";");
+            return 1;
+        } catch (SQLException e) {
+      
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+
+
+
+    private String parseUpdateField(UserModelFiledsEnum fieldToParse) {
+        switch (fieldToParse) {
+
+            case USERNAME:
+                return "username";
+            case PASSWORD:
+                return "passwrd";
+            case EMAIL:
+                return "email";
+            case PREMIUM:
+                return "premium";
+            case WISHLIST:
+                return "wishlist";
+            case PREFERENCES:
+                return "preferences";
+            case TIER:
+                return "tier";
+            case MESSAGES:
+                return "messages";
+            case ADS:
+                return "ads";
+            default:
+                return "";
+        }
+    }
+
+    private String normalizeStringArrays(ArrayList<String> arrayToParse) {
+
+        String parsedString = "";
+
+        for (String currString : arrayToParse) {
+            parsedString += currString + ",";
+        }
+
+        parsedString = parsedString.substring(0, parsedString.length() - 1);
+
+        return parsedString;
+    }
+
+    private boolean searchUserByUsername(String usernameToSearch) {
+
+        try {
+            while (this.usersCollection.next()) {
+                if (this.usersCollection.getString("username").equals(usernameToSearch)) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+      
+
+    }
+
+    private boolean searchUserByEmail(String emailToSearch){
+
+        try {
+            while (this.usersCollection.next()) {
+                if (this.usersCollection.getString("email").equals(emailToSearch)) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+  
 
     public Connection getDBConnection(){
         return this.dbConnection;
