3b6e308184926af38f45f1cdd0ece5232f20550f
ktsourdi <kyrosts@gmail.com>
2020-05-19 15:45:29 +0300

    registered logout

--- src/uniShop/Registered.java
+++ src/uniShop/Registered.java
@@ -2,7 +2,7 @@ package uniShop;
 
 public class Registered extends User {
 	
-	
+	int status=1; //1 για συνδεδεμένο
 
 	public Registered(int userId, String username, String email) {
 		super(userId, username, email);
@@ -10,5 +10,9 @@ public class Registered extends User {
 	}
 	
 	
+	public void logout()
+	{
+		status = 0; //0 για αποσυνδεδεμένο
+	};
 
 }
