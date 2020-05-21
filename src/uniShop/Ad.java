5072ddcd8038d62ba3491508df4b4352ac9710b9
xristosmitr <xristosmitragas@gmail.com>
2020-05-21 20:53:31 +0300

    Ad and Message class fields

---
+++ src/uniShop/Ad.java
@@ -0,0 +1,19 @@
+package uniShop;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Ad {
+
+	private int ad_id;
+	private String name;
+	private String description;
+	private String photoLink;
+	private Date date;
+	private Registered seller;
+	private Registered buyer;
+	private ArrayList<String> tags;
+	private ListingState status;
+	private ListingPromotionType promotionType;
+	
+	
+}
---
+++ src/uniShop/Message.java
@@ -0,0 +1,11 @@
+package uniShop;
+
+public class Message {
+	
+	private String text;
+	private Registered sender;
+	private Registered receiver;
+	private Ad product ;
+
+	
+}
