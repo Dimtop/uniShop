80c98f26b39bc6b11fda66c0b69eab1a65bfe217
xristosmitr <xristosmitragas@gmail.com>
2020-05-21 21:10:12 +0300

    Getters,Setters and methods of Ad and Message

--- src/uniShop/Ad.java
+++ src/uniShop/Ad.java
@@ -12,8 +12,98 @@ public class Ad {
 	private Registered seller;
 	private Registered buyer;
 	private ArrayList<String> tags;
-	private ListingState status;
-	private ListingPromotionType promotionType;
+	private ListingState status; //Ad status
+	private ListingPromotionType promotionType;//Type of promoted ad
 	
+	public int getAd_id() 
+	{
+		return ad_id;
+	}
+	public void setAd_id(int ad_id) 
+	{
+		this.ad_id = ad_id;
+	}
+	public String getName() 
+	{
+		return name;
+	}
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	public String getDescription() 
+	{
+		return description;
+	}
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+	public String getPhotoLink() 
+	{
+		return photoLink;
+	}
+	public void setPhotoLink(String photoLink)
+	{
+		this.photoLink = photoLink;
+	}
+	public Date getDate()
+	{
+		return date;
+	}
+	public void setDate(Date date) 
+	{
+		this.date = date;
+	}
+	public Registered getSeller() 
+	{
+		return seller;
+	}
+	public void setSeller(Registered seller) 
+	{
+		this.seller = seller;
+	}
+	public Registered getBuyer() 
+	{
+		return buyer;
+	}
+	public void setBuyer(Registered buyer)
+	{
+		this.buyer = buyer;
+	}
+	public ArrayList<String> getTags() 
+	{
+		return tags;
+	}
+	public void setTags(ArrayList<String> tags) 
+	{
+		this.tags = tags;
+	}
+	public ListingState getStatus() 
+	{
+		return status;
+	}
+	public ListingState changeStatus(ListingState status)
+	{
+		this.status = status;
+	}
+	public ListingPromotionType getPromotionType() 
+	{
+		return promotionType;
+	}
+	public void setPromotionType(ListingPromotionType promotionType) 
+	{
+		this.promotionType = promotionType;
+	}
+	
+	public void addTag(String aTag)
+	{
+		tags.add(aTag);
+	}
+	
+	public void removeTag(String aTag)
+	{
+		tags.remove(aTag);
+	}
 	
 }
--- src/uniShop/Message.java
+++ src/uniShop/Message.java
@@ -6,6 +6,40 @@ public class Message {
 	private Registered sender;
 	private Registered receiver;
 	private Ad product ;
+	
+	public String getText() 
+	{
+		return text;
+	}
+	public void setText(String text) 
+	{
+		this.text = text;
+	}
+	public Registered getSender() 
+	{
+		return sender;
+	}
+	public void setSender(Registered sender) 
+	{
+		this.sender = sender;
+	}
+	public Registered getReceiver() 
+	{
+		return receiver;
+	}
+	public void setReceiver(Registered receiver) 
+	{
+		this.receiver = receiver;
+	}
+	public Ad getProduct() 
+	{
+		return product;
+	}
+	public void setProduct(Ad product) 
+	{
+		this.product = product;
+	}
 
+	
 	
 }
