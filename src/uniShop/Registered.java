78bb2a6c4c640fc099f46bd6f709a5d122445e69
ktsourdi <kyrosts@gmail.com>
2020-05-23 16:23:41 +0300

    προσθήκη τοπικής/υποθετικής βάσης με ads δημιουργία create ad, delete ad

--- src/uniShop/Registered.java
+++ src/uniShop/Registered.java
@@ -1,9 +1,37 @@
 package uniShop;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*; 
+
 public class Registered extends User {
 	
-	int status=1; //1 για συνδεδεμένο
+	int status=1; //κατασταση χρήστη, συνδεδεμένος/μη συνδεδεμένος
+	
+	
+	//παρακάτω φτιάχνω μια τοπική βάση απο ads
+	
+	ArrayList<String> tags1 = 
+			new ArrayList<>(Arrays.asList("iphone", "phone", "6s"));
+	
+	ArrayList<String> tags2 = 
+			new ArrayList<>(Arrays.asList("hp", "laptop", "pc"));
+	
+	ArrayList<String> tags3 = 
+			new ArrayList<>(Arrays.asList("chair", "new", "confortable"));
+	
+	
+	
+	Ad ad1 = new Ad(22, "iphone 6s", tags1);
+	Ad ad2 = new Ad(13, "Laptop HP", tags2);
+	Ad ad3 = new Ad(222, "new chair", tags3);
+	
+	ArrayList<Ad> aDataBase = new ArrayList<Ad>(Arrays.asList(ad1, ad2, ad3));
 
+	//τέλος δημιουργίας βάσης
+	
+	
+	
 	public Registered(int userId, String username, String email) {
 		super(userId, username, email);
 		
@@ -13,8 +41,31 @@ public class Registered extends User {
 	public void logout()
 	{
 		status = 0; //0 για αποσυνδεδεμένο
+		new Guest(userId, username, email); 
 	};
 	
-	public void veiwProfile() {};
-
+	public void viewProfile() {};
+	
+	
+		
+		
+	public void createAd()
+	{	 
+		ArrayList<String> tags = 
+				new ArrayList<>(Arrays.asList("phone", "samsung", "galaxy"));
+		Ad anAd = new Ad(555, "samsung galaxy", tags);
+		aDataBase.add(anAd);
+	};
+	
+	
+	public void deleteAd(Ad anAd)
+	{
+		
+		anAd = ad1; //θεωρώ οτι υπάρχει ηδη στην βάση 
+		aDataBase.remove(anAd);
+	};
+	
+	
+	
+	
 }
