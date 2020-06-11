4f9ebebfa75570366a1614d629c1458315c315ec
tzavachil <tzav.achil@yahoo.gr>
2020-06-11 20:33:33 +0300

    Fixing photo links

--- src/uniShop/LocalDataBase.java
+++ src/uniShop/LocalDataBase.java
@@ -105,71 +105,71 @@ public class LocalDataBase {
 		Ad ad1 = new Ad(nextAdId, "Product 1", descriptionText + "1", ad1Link, currDate, 
 				this.systemUsers.get(1), this.systemUsers.get(2), this.getSomePreferences(2), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad2Link = "https://www.amazon.com/SAMSUNG-75-inch-Crystal-TU-8000-Built/dp/B084JBN6W9/ref=gbps_img_m-9_475e_6aa0e70d?smid=ATVPDKIKX0DER&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad2Link = "https://images-na.ssl-images-amazon.com/images/I/71RiQZ0J2SL._AC_SL1000_.jpg";
 		Ad ad2 = new Ad(nextAdId, "Product 2", descriptionText + "2", ad2Link, currDate, 
 				this.systemUsers.get(2), this.systemUsers.get(3), this.getSomePreferences(3), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad3Link = "https://www.amazon.com/Bedsure-Sherpa-Fleece-Blanket-Microfiber/dp/B0747BDTBD/ref=gbps_img_m-9_475e_50a2659a?smid=A3LDROY0BE4ISH&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad3Link = "https://images-na.ssl-images-amazon.com/images/I/81n%2BlyIdOVL._AC_SL1500_.jpg";
 		Ad ad3 = new Ad(nextAdId, "Product 3", descriptionText + "3", ad3Link, currDate, 
 				this.systemUsers.get(3), this.systemUsers.get(4), this.getSomePreferences(1), ListingState.ON_HOLD);
 		this.nextAdId++;
-		String ad4Link = "https://www.amazon.com/s/browse/ref=gbps_img_m-9_475e_ce1a3533?ie=UTF8&node=12223424011&smid=ATVPDKIKX0DER&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad4Link = "https://images-na.ssl-images-amazon.com/images/I/714Y-S1r7ML._AC_SL1500_.jpg";
 		Ad ad4 = new Ad(nextAdId, "Product 4", descriptionText + "4", ad4Link, currDate, 
 				this.systemUsers.get(5), this.systemUsers.get(7), this.getSomePreferences(1), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad5Link = "https://www.amazon.com/gp/slredirect/picassoRedirect.html?qualifier=1591891420&id=8564832007589610&adId=A00227111MYAT5ZDTTYMR&widgetName=sp_gb_main_supple&url=https%3A%2F%2Fwww.amazon.com%2FPURA-DOR-Energizing-Revitalizer-Ingredients%2Fdp%2FB07J55YHGD%2Fref%3Dgbps_img_m-9_475e_7f87089f%3Fsmid%3DA2W9BJCKST9EWU%26pf_rd_p%3D5d86def2-ec10-4364-9008-8fbccf30475e%26pf_rd_s%3Dmerchandised-search-9%26pf_rd_t%3D101%26pf_rd_i%3D15529609011%26pf_rd_m%3DATVPDKIKX0DER%26pf_rd_r%3DNNH0B066VC80VVSDKKBC";
+		String ad5Link = "https://images-na.ssl-images-amazon.com/images/I/71iV%2BfLoJCL._AC_SL1500_.jpg";
 		Ad ad5 = new Ad(nextAdId, "Product 5", descriptionText + "5", ad5Link, currDate, 
 				this.systemUsers.get(8), this.systemUsers.get(10), this.getSomePreferences(4), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad6Link = "https://www.amazon.com/UBeesize-Ringlight-YouTube-Photography-Compatible/dp/B07GDC39Y2/ref=gbps_img_m-9_475e_e388c56a?smid=A3JPZCPPKKCO75&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad6Link = "https://images-na.ssl-images-amazon.com/images/I/91a2QToQOIL._SL1500_.jpg";
 		Ad ad6 = new Ad(nextAdId, "Product 6", descriptionText + "6", ad6Link, currDate, 
 				this.systemUsers.get(10), this.systemUsers.get(1), this.getSomePreferences(2), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad7Link = "https://www.amazon.com/Bare-Home-Fitted-Bottom-Sheet/dp/B011W64E8O/ref=gbps_img_m-9_475e_e6e27391?smid=A3UBXJ2R9SZ9Z4&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad7Link = "https://images-na.ssl-images-amazon.com/images/I/61Nwtf2xc6L._AC_SL1500_.jpg";
 		Ad ad7 = new Ad(nextAdId, "Product 7", descriptionText + "7", ad7Link, currDate, 
 				this.systemUsers.get(3), this.systemUsers.get(7), this.getSomePreferences(3), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad8Link = "https://www.amazon.com/TENVIS-Wireless-Surveillance-Security-Auto-Cruise/dp/B071DDBT7M/ref=gbps_img_m-9_475e_f00cac82?smid=A18FTUOLO656PB&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad8Link = "https://images-na.ssl-images-amazon.com/images/I/71aPkUJQmFL._AC_SL1500_.jpg";
 		Ad ad8 = new Ad(nextAdId, "Product 8", descriptionText + "8", ad8Link, currDate, 
 				this.systemUsers.get(10), this.systemUsers.get(4), this.getSomePreferences(4), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad9Link = "https://www.amazon.com/deal/80c31c43/ref=gbps_img_m-9_475e_80c31c43?showVariations=true&smid=ATVPDKIKX0DER&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad9Link = "https://images-na.ssl-images-amazon.com/images/I/51Se9eTnyNL._AC_SL1000_.jpg";
 		Ad ad9 = new Ad(nextAdId, "Product 9", descriptionText + "9", ad9Link, currDate, 
 				this.systemUsers.get(2), this.systemUsers.get(10), this.getSomePreferences(7), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad10Link = "https://www.amazon.com/PURA-DOR-Anti-Thinning-Moisturizing-Conditioner/dp/B07BHCB9Y2/ref=gbps_img_m-9_475e_170fee72?smid=A2W9BJCKST9EWU&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad10Link = "https://images-na.ssl-images-amazon.com/images/I/71GdlYQC1UL._AC_SL1100_.jpg";
 		Ad ad10 = new Ad(nextAdId, "Product 10", descriptionText + "10", ad10Link, currDate, 
 				this.systemUsers.get(5), this.systemUsers.get(6), this.getSomePreferences(1), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad11Link = "https://www.amazon.com/deal/8db4468d/ref=gbps_img_m-9_475e_8db4468d?smid=ATVPDKIKX0DER&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad11Link = "https://images-na.ssl-images-amazon.com/images/I/81nuCG0VLtL._AC_SL1500_.jpg";
 		Ad ad11 = new Ad(nextAdId, "Product 11", descriptionText + "11", ad11Link, currDate, 
 				this.systemUsers.get(7), this.systemUsers.get(3), this.getSomePreferences(2), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad12Link = "https://www.amazon.com/BEBONCOOL-Controller-DualShock-Playstation-PlayStation4/dp/B071WNWHZ7/ref=gbps_img_m-9_475e_398dc6b3?smid=ATD293HIUAW1A&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad12Link = "https://images-na.ssl-images-amazon.com/images/I/61WqyW4li2L._AC_SL1000_.jpg";
 		Ad ad12 = new Ad(nextAdId, "Product 12", descriptionText + "12", ad12Link, currDate, 
 				this.systemUsers.get(1), this.systemUsers.get(10), this.getSomePreferences(2), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad13Link = "https://www.amazon.com/Miusey-Sleeveless-Classic-Versatile-Textured/dp/B079BNM8C1/ref=gbps_img_m-9_475e_3e8e6d3d?smid=A1YYI53EEY1YMY&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad13Link = "https://images-na.ssl-images-amazon.com/images/I/81oEB3azOrL._AC_SL1500_.jpg";
 		Ad ad13 = new Ad(nextAdId, "Product 13", descriptionText + "13", ad13Link, currDate, 
 				this.systemUsers.get(2), this.systemUsers.get(4), this.getSomePreferences(4), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad14Link = "https://www.amazon.com/Cleansing-PIXNOR-Waterproof-Exfoliating-Blackhead/dp/B07SPJJ7D5/ref=gbps_img_m-9_475e_3695e444?smid=A1XQWCA8UWTYMI&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad14Link = "https://images-na.ssl-images-amazon.com/images/I/81vQEfcB29L._AC_SL1500_.jpg";
 		Ad ad14 = new Ad(nextAdId, "Product 14", descriptionText + "14", ad14Link, currDate, 
 				this.systemUsers.get(3), this.systemUsers.get(7), this.getSomePreferences(2), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad15Link = "https://www.amazon.com/Boyata-Multi-Angle-Heat-Vent-Adjustable-Compatible/dp/B07WRC9B86/ref=gbps_img_m-9_475e_d4dd6ffa?smid=ACDXJI6L8IN7K&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad15Link = "https://images-na.ssl-images-amazon.com/images/I/81ZB62prn-L._AC_SL1500_.jpg";
 		Ad ad15 = new Ad(nextAdId, "Product 15", descriptionText + "15", ad15Link, currDate, 
 				this.systemUsers.get(9), this.systemUsers.get(4), this.getSomePreferences(5), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad16Link = "https://www.amazon.com/Upgraded-Wireless-Controller-KUTIME-Nintendo/dp/B081Q33VQT/ref=gbps_img_m-9_475e_53636850?smid=A2EW7E9HR4F2AI&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad16Link = "https://i.ebayimg.com/images/g/aEoAAOSw6hZcnWYe/s-l300.png";
 		Ad ad16 = new Ad(nextAdId, "Product 16", descriptionText + "16", ad16Link, currDate, 
 				this.systemUsers.get(5), this.systemUsers.get(3), this.getSomePreferences(4), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad17Link = "https://www.amazon.com/sponeed-Cycling-Compresson-Underwear-Coolmax/dp/B01LY3R2I6/ref=gbps_img_m-9_475e_af38cde4?smid=A2WLIKPPW4D7PD&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad17Link = "https://images-na.ssl-images-amazon.com/images/I/71MNCkCo82L._AC_SL1500_.jpg";
 		Ad ad17 = new Ad(nextAdId, "Product 17", descriptionText + "17", ad17Link, currDate, 
 				this.systemUsers.get(8), this.systemUsers.get(9), this.getSomePreferences(3), ListingState.ACTIVE);
 		this.nextAdId++;
-		String ad18Link = "https://www.amazon.com/deal/7b0a60f2/ref=gbps_img_m-9_475e_7b0a60f2?&searchAlias=fashion&smid=ATVPDKIKX0DER&pf_rd_p=5d86def2-ec10-4364-9008-8fbccf30475e&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=NNH0B066VC80VVSDKKBC";
+		String ad18Link = "https://images-na.ssl-images-amazon.com/images/I/71wSuNVCtvL._AC_SL1500_.jpg";
 		Ad ad18 = new Ad(nextAdId, "Product 18", descriptionText + "18", ad18Link, currDate, 
 				this.systemUsers.get(3), this.systemUsers.get(6), this.getSomePreferences(1), ListingState.ACTIVE);
 		this.nextAdId++;
@@ -310,4 +310,17 @@ public class LocalDataBase {
 				this.systemAds.remove(currUser);
 		}
 	}
+
+	public ArrayList<Registered> getSystemUsers() {
+		return systemUsers;
+	}
+
+	public ArrayList<Ad> getSystemAds() {
+		return systemAds;
+	}
+
+	public ArrayList<String> getSystemPreferences() {
+		return systemPreferences;
+	}
+
 }
